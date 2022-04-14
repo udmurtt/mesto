78fd5c376d3c6dfa30f338e619a402d99c6d4336
Sergey Eliseev <eliseev.s3rzh@yandex.ru>
2022-04-15 00:56:44 +0400

    итерациядваисправитьjs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,33 +1,28 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popup.querySelector('.popup__close');
-const saveButton = popup.querySelector('.popup__save');
-let namecontent = document.querySelector('.profile__name').textContent;
-let aboutmecontent = document.querySelector('.profile__about-me').textContent;
+let namecontent = document.querySelector('.profile__name');
+let aboutmecontent = document.querySelector('.profile__about-me');
 let form = popup.querySelector('.popup__conteiner');
 let inputname = form.querySelector('.popup__user-name');
 let inputbio = form.querySelector('.popup__user-bio');
 
-function popupOpenToggle() {
-    popup.classList.toggle('popup_is-opened');
-    popup.querySelector('.popup__user-name').value = namecontent;
-    popup.querySelector('.popup__user-bio').value = aboutmecontent;
+function popupOpen() {
+    popup.classList.add('popup_is-opened');
+    popup.querySelector('.popup__user-name').value = namecontent.textContent;
+    popup.querySelector('.popup__user-bio').value = aboutmecontent.textContent;
 }
-editButton.addEventListener('click', popupOpenToggle);
+editButton.addEventListener('click', popupOpen);
 
-function popupCloseToggle() {
-	popup.classList.toggle('popup_is-opened');
+function popupClose() {
+	popup.classList.remove('popup_is-opened');
 }
-closeButton.addEventListener('click', popupCloseToggle);
-
-function popupSaveToggle() {
-    popup.classList.toggle('popup_is-opened');
-}
-saveButton.addEventListener('click', popupSaveToggle);
+closeButton.addEventListener('click', popupClose);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    document.querySelector('.profile__name').textContent = inputname.value;
-    document.querySelector('.profile__about-me').textContent = inputbio.value;
+    namecontent.textContent = inputname.value;
+    aboutmecontent.textContent = inputbio.value;
+		popupClose();
 }
 form.addEventListener('submit', formSubmitHandler); 
