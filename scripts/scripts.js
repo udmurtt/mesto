1815c9bf8407babe1714e9af3fee2097c1030050
Sergey Eliseev <eliseev.s3rzh@yandex.ru>
2022-04-14 23:40:31 +0400

    ревьюперваяитерация

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,26 +2,28 @@ const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popup.querySelector('.popup__close');
 const saveButton = popup.querySelector('.popup__save');
-
-
-function popupOpenToggle() {
-    popup.classList.toggle('popup_closed');
-    let namecontent = document.querySelector('.profile__name').textContent;
-    let aboutmecontent = document.querySelector('.profile__about-me').textContent;
-    popup.querySelector('.popup__user-name').value = namecontent;
-    popup.querySelector('.popup__user-bio').value = aboutmecontent;
-}
-editButton.addEventListener('click', popupOpenToggle)
-closeButton.addEventListener('click', popupOpenToggle)
-
-function popupSaveToggle() {
-    popup.classList.toggle('popup_closed');
-}
-saveButton.addEventListener('click', popupSaveToggle)
-
+let namecontent = document.querySelector('.profile__name').textContent;
+let aboutmecontent = document.querySelector('.profile__about-me').textContent;
 let form = popup.querySelector('.popup__conteiner');
 let inputname = form.querySelector('.popup__user-name');
 let inputbio = form.querySelector('.popup__user-bio');
+
+function popupOpenToggle() {
+    popup.classList.toggle('popup_is-opened');
+    popup.querySelector('.popup__user-name').value = namecontent;
+    popup.querySelector('.popup__user-bio').value = aboutmecontent;
+}
+editButton.addEventListener('click', popupOpenToggle);
+
+function popupCloseToggle() {
+	popup.classList.toggle('popup_is-opened');
+}
+closeButton.addEventListener('click', popupCloseToggle);
+
+function popupSaveToggle() {
+    popup.classList.toggle('popup_is-opened');
+}
+saveButton.addEventListener('click', popupSaveToggle);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
