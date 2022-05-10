02f75a24db06ef7f796135d6ff9e1c65b59d8b30
Sergey Eliseev <eliseev.s3rzh@yandex.ru>
2022-05-11 02:25:24 +0400

    реализовать попап с картикой

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -57,7 +57,7 @@ const initialCards = [
 // дом элементы
 
 const cardsConteiner = document.querySelector('.elements');
-const popupForCardsEdit = document.querySelector('.popup-cards');
+const popupForCardsEdit = document.querySelector('.popup_cards');
 const cardsEditButton = document.querySelector('.profile__add-button');
 const cardsEditCloseBatton = popupForCardsEdit.querySelector('.popup__close');
 const cardsEditForm = popupForCardsEdit.querySelector('.popup__conteiner');
@@ -69,7 +69,7 @@ const renderInitialCards = (initialCardsData) => {
 		cardsConteiner.insertAdjacentHTML('afterbegin', `
 		<div class="element">
 								<button class="element__delete" type="button" aria-label="удалить карточку"></button>
-                <img class="element__image" src="${initialCardsData.link}" alt="далёкая гора">
+                <img class="element__image" src="${initialCardsData.link}" alt="">
                 <div class="element__footer">
                     <h2 class="element__title">${initialCardsData.name}</h2>
                     <button class="element__like" type="button" aria-label="поставить лайк"></button>
@@ -81,8 +81,17 @@ const renderInitialCards = (initialCardsData) => {
 		function cardIsLiked(evt) {
 			evt.target.closest('.element__like').classList.toggle('element__like_active');
 		}
+    const cardDeleteButton = document.querySelector('.element__delete');
+    cardDeleteButton.addEventListener('click', handleDeleteCard);
+
+    const cardImg = document.querySelector('.element__image');
+    cardImg.addEventListener('click', popupImgOpen);
+    //const cardImgTitle =document.querySelector('.element__title');
 }
 
+    const handleDeleteCard = (event) => {
+      event.target.closest('.element').remove();
+    }
 
 initialCards.forEach((initialCardsData) => {
 		renderInitialCards(initialCardsData);
@@ -104,4 +113,22 @@ function cardsEditFormSubmitHandler (evt) {
 	popupForCardsEditClose();
 }
 cardsEditForm.addEventListener('submit', cardsEditFormSubmitHandler);
+
+const popupImg = document.querySelector('.popup_img');
+const popupImgBacground = popupImg.querySelector('.popup__image');
+const popupImgTitle = popupImg.querySelector('.popup__img-title');
+const popupImgCloseBatton = popupImg.querySelector('.popup__close');
+
+function popupImgOpen(event) {
+	popupImg.classList.add('popup_is-opened');
+  popupImgBacground.src = event.target.closest('.element__image').src;
+  popupImgTitle.textContent = event.target.closest('.element').querySelector('.element__title').textContent;
+}
+
+
+function popupImgClose() {
+	popupImg.classList.remove('popup_is-opened');
+}
+
+popupImgCloseBatton.addEventListener('click', popupImgClose);
 
